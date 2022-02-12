efcf95c7fb1f8f212c2a20abc09d46ed022fbe67
mhrgoldberg <mitchellreiss@gmail.com>
2022-02-12 15:02:54 -0800

    New: example backend code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next"
+import helloWorld from "../../lib"
 
 type Data = {
-  name: string
+  message: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+// http://localhost:3000/api/hello?name=Zaviar => { message: 'Hello World, Zaviar!' }
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ message: helloWorld(req.query.name as string) })
 }
