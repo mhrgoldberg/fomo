a79a979e9ff90c50e2e53d008ba618dc9e0aadb1
mhrgoldberg <mitchellreiss@gmail.com>
2022-04-28 12:50:53 -0700

    Feat: Setup NextAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,30 @@
+import { SessionProvider } from "next-auth/react"
 import type { AppProps } from "next/app"
 import "normalize.css/normalize.css"
 import "../styles/globals.css"
+import SessionWrapper from "./api/auth/SessionWrapper"
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface CustomAppProps {
+  Component: AppProps["Component"] & {
+    auth?: boolean
+  }
+  pageProps: AppProps["pageProps"]
+}
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: CustomAppProps) {
+  return (
+    <SessionProvider session={session}>
+      {Component.auth ? (
+        // If Component has auth property will render loading page while checking session
+        <SessionWrapper>
+          <Component {...pageProps} />
+        </SessionWrapper>
+      ) : (
+        // Render Component without loading page
+        <Component {...pageProps} />
+      )}
+    </SessionProvider>
+  )
 }
 
 export default MyApp
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 import GithubProvider from "next-auth/providers/github"
 import prisma from "../../../lib/db"
+import { checkPassword } from "../../../lib/user/auth"
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -9,6 +11,29 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        })
+        if (!user?.password) return null
+        const valid = await checkPassword(credentials.password, user.password)
+        if (!valid) return null
+
+        return user
+      },
     }),
   ],
 })
